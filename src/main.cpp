ec050cc1f189cfedd2665086299f82b5e44efbfe
Daniel Aimé Valcour <60055347+Journeyman-dev@users.noreply.github.com>
2023-06-28 21:46:30 -0400

    Finish "Hello Window"

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,84 @@
 //
 // SPDX-License-Identifier: CC-BY-4.0
 
+#include <glad/glad.h>
+#define GLFW_INCLUDE_NONE
+#include <GLFW/glfw3.h>
+
 #include <iostream>
 
 int main()
 {
-  std::cout << "Hello, world!" << std::endl;
+  if (!glfwInit())
+  {
+    std::cout << "failed to initialize glfw" << std::endl;
+  }
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+#if defined(__APPLE__)
+  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+
+  const int initial_width = 800;
+  const int initial_height = 600;
+
+  GLFWwindow* window =
+    glfwCreateWindow(
+      initial_width,
+      initial_height,
+      "LearnOpenGL",
+      NULL,
+      NULL
+    );
+  if (window == NULL)
+  {
+    std::cout << "Failed to create GLFW window" << std::endl;
+    glfwTerminate();
+    return -1;
+  }
+  glfwMakeContextCurrent(window);
+
+  glfwSetFramebufferSizeCallback(
+    window,
+    [](GLFWwindow* window, int width, int height)
+    {
+      glViewport(0, 0, width, height);
+    }
+  );
+
+  glfwSetKeyCallback(
+    window,
+    [](GLFWwindow* window, int key, int scancode, int action, int mods)
+    {
+      if(
+        key == GLFW_KEY_ESCAPE &&
+        action == GLFW_PRESS
+      )
+      {
+        glfwSetWindowShouldClose(window, true);
+      }
+    }
+  );
+
+  glViewport(0, 0, initial_width, initial_height);
+
+  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+  {
+    std::cout << "Failed to initialize GLAD" << std::endl;
+    return -1;
+  }
+
+  while(!glfwWindowShouldClose(window))
+  {
+    glfwPollEvents();
+
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glfwSwapBuffers(window);
+  }
+
+  glfwTerminate();
   return 0;
 }
